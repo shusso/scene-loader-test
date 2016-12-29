24dd1bee4a99262b0ac6de716b3d4e41f044558f
Samuel Husso <samuel@unity3d.com>
2016-12-29 20:27:15 +0200

    initial commit

---
+++ Assets/Scripts/AdsManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Advertisements;
+
+public class AdsManager : MonoBehaviour {
+	string gameIdIOS = "1249251";
+	string gameIdAndroid = "1249252";
+
+	private void HandleShowResults(ShowResult result) {
+		switch(result) {
+			case ShowResult.Finished:
+				Debug.Log("Ads Finished");
+				break;
+			case ShowResult.Skipped:
+				Debug.Log("Ads skipped");
+				break;
+			case ShowResult.Failed:
+				Debug.Log("Ads failed");
+				break;
+		}
+	}
+
+	public void ShowAds() {
+		if (Advertisement.IsReady()) {
+			var options = new ShowOptions{resultCallback = HandleShowResults };
+			Advertisement.Show("video", options);
+		}
+		else {
+			Debug.Log("No Ads to show..");
+		}
+	}
+
+	void Start () {
+#if UNITY_IOS
+		Debug.Log("Initialized Ads for iOS");
+		Advertisement.Initialize(gameIdIOS);
+#elif UNITY_ANDROID
+		Debug.Log("Initialized Ads for Android");
+		Advertisement.Initialize(gameIdAndroid);
+#endif
+
+	}
+
+	void Update () {
+
+	}
+}
---
+++ Assets/Scripts/ObjectCreator.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+
+public class ObjectCreator : MonoBehaviour {
+
+	private Vector3 m_SpawnPoint;
+	public GameObject m_SpawnObject;
+	public int m_SpawnCount = 50;
+
+	private int m_SpawnedObjects = 0;
+	public GameObject UITextComponent;
+	public InputField UISpawnCountComponent;
+	private Text UIText;
+
+	public Vector3 GetRandomSpawnPoint() {
+		var spawnPoint = new Vector3(UnityEngine.Random.Range(0,Screen.width), UnityEngine.Random.Range(0,Screen.height), Camera.main.farClipPlane/2);
+		return Camera.main.ScreenToWorldPoint(spawnPoint);
+	}
+
+	public void UIWriteSpawnCount() {
+		UIText.text = "Spawned Objects: " + m_SpawnedObjects;
+	}
+
+	public void SpawnObject() {
+		Debug.Log("Spawn item to screen");
+
+		var location = GetRandomSpawnPoint();
+
+		Instantiate(m_SpawnObject, location, Quaternion.identity);
+
+		m_SpawnedObjects += 1;
+
+	}
+
+	public void SpawnObjects() {
+		for (int i = 1; i <= m_SpawnCount; i++) {
+			var location = GetRandomSpawnPoint();
+			Instantiate(m_SpawnObject, location, Quaternion.identity);
+		}
+		m_SpawnedObjects += m_SpawnCount;
+	}
+
+	public void UISetSpawnCount(string arg0) {
+		var count = Convert.ToInt32(arg0);
+		Debug.Log("SpawnCount changed to " + count.ToString());
+		m_SpawnCount = count;
+	}
+
+	void Start() {
+		UIText = UITextComponent.GetComponent<Text>();
+		UISpawnCountComponent.onEndEdit.AddListener(UISetSpawnCount);
+	}
+}
